a1918574380dfc2cef31bc6358bf0041ef93eeb1
jaepyo99 <jaepyo99@naver.com>
2024-03-16 12:50:37 +0900

    Init: 프로젝트 초기 설정

---
+++ src/main/java/com/univ/haksamo/HaksamoApplication.java
@@ -0,0 +1,13 @@
+package com.univ.haksamo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaksamoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaksamoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/univ/haksamo/HaksamoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.univ.haksamo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaksamoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
