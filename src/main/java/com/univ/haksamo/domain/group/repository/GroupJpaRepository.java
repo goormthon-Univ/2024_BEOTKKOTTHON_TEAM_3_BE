855834546e5dc2dac82676b34e80e12787ba6ccc
jaepyo99 <jaepyo99@naver.com>
2024-03-20 16:46:55 +0900

    feat: 대학내의 가입된 학과 목록 전체 조회
    대학내의 가입된 학과의 id와 name값 응답
    
    Github Issue : #23

--- src/main/java/com/univ/haksamo/domain/group/repository/GroupJpaRepository.java
+++ src/main/java/com/univ/haksamo/domain/group/repository/GroupJpaRepository.java
@@ -3,5 +3,8 @@ package com.univ.haksamo.domain.group.repository;
 import com.univ.haksamo.domain.group.entity.Group;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface GroupJpaRepository extends JpaRepository<Group,Long> {
+    List<Group> findAllByUniversityId(Long universityId);
 }
