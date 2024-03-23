53f6702cd1300cd69ed2b1c13f0aabc3d9083cc8
uch0731 <uch0731@naver.com>
2024-03-24 02:28:15 +0900

    doc : 유저 키워드 swagger 추가

--- src/main/java/com/univ/haksamo/domain/bookmark/controller/UserKeyWordController.java
+++ src/main/java/com/univ/haksamo/domain/bookmark/controller/UserKeyWordController.java
@@ -3,17 +3,20 @@ package com.univ.haksamo.domain.bookmark.controller;
 
 import com.univ.haksamo.domain.bookmark.service.UserKeywordService;
 import com.univ.haksamo.global.format.success.SuccessResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "유저 키워드 관련 api")
 @RestController
 @RequiredArgsConstructor
 public class UserKeyWordController {
     private final UserKeywordService userKeywordService;
 
-
+    @Operation(summary = "로그인 했을때 키워드 목록 가져오는 api")
     @PostMapping("/haksamo/first/users/keywords")
     public SuccessResponse<String> FirstLoginKeywordController() {
         userKeywordService.FirstLoginSaveKeyword();
