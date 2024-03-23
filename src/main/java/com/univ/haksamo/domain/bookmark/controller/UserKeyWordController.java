4c584dac55a120815aadd375aaf069571577ebf7
uch0731 <uch0731@naver.com>
2024-03-24 02:36:18 +0900

    refactor : 키워드 저장 login 에서 signup 후로 수정

--- src/main/java/com/univ/haksamo/domain/bookmark/controller/UserKeyWordController.java
+++ src/main/java/com/univ/haksamo/domain/bookmark/controller/UserKeyWordController.java
@@ -7,7 +7,6 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Tag(name = "유저 키워드 관련 api")
@@ -16,10 +15,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserKeyWordController {
     private final UserKeywordService userKeywordService;
 
-    @Operation(summary = "로그인 했을때 키워드 목록 가져오는 api")
-    @PostMapping("/haksamo/first/users/keywords")
-    public SuccessResponse<String> FirstLoginKeywordController() {
-        userKeywordService.FirstLoginSaveKeyword();
+    @Operation(summary = "회원가입하면 키워드 목록 가져오는 api")
+    @PostMapping("/haksamo/users/keywords")
+    public SuccessResponse<String> saveUserKeywordController() {
+        userKeywordService.saveUserKeyword();
         return SuccessResponse.ok();
     }
 
