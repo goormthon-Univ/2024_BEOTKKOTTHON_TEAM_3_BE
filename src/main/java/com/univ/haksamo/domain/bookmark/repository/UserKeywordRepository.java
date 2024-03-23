ef6174cc3a418da82ee4d6eba5e3469753329a6b
jaepyo99 <jaepyo99@naver.com>
2024-03-24 04:21:00 +0900

    fix: 즐겨찾기한 키워드 수정
    Github Issue : #

--- src/main/java/com/univ/haksamo/domain/bookmark/repository/UserKeywordRepository.java
+++ src/main/java/com/univ/haksamo/domain/bookmark/repository/UserKeywordRepository.java
@@ -4,12 +4,13 @@ import com.univ.haksamo.domain.bookmark.entity.UserKeword;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UserKeywordRepository extends JpaRepository<UserKeword, Long> {
     List<UserKeword> findAllByUserId(Long userId);
 
     List<UserKeword> findAllByKeywordId(Long keywordId);
 
-    UserKeword findAllByUserIdAndKeywordId(Long userId,Long keywordId);
+    Optional<UserKeword> findAllByUserIdAndKeywordId(Long userId, Long keywordId);
 
 }
