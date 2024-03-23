116b41b909487a25984d059c1f34dd7c44817053
uch0731 <uch0731@naver.com>
2024-03-23 21:01:07 +0900

    docs : 게시글 컨트롤러 swagger api 설명 추가

--- src/main/java/com/univ/haksamo/domain/board/controller/BoardWriteController.java
+++ src/main/java/com/univ/haksamo/domain/board/controller/BoardWriteController.java
@@ -3,6 +3,8 @@ package com.univ.haksamo.domain.board.controller;
 import com.univ.haksamo.domain.board.controller.req.WriteBoardRequestDto;
 import com.univ.haksamo.domain.board.service.BoardWriteService;
 import com.univ.haksamo.global.format.success.SuccessResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -10,12 +12,15 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.util.List;
 
+@Tag(name = "공지글 쓰기 api")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("${api.prefix.admin}")
 public class BoardWriteController {
 
     private final BoardWriteService boardWriteService;
+
+    @Operation(summary = "공지글 작성 api")
     @PostMapping("/groups/{groupId}/boards")
     public SuccessResponse<String> write(@PathVariable Long groupId,
                                  @RequestPart List<MultipartFile> images,
