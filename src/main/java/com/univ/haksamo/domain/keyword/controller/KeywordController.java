3095fbd6d64d96bbc4f05e578b0b00e8b29b7312
uch0731 <uch0731@naver.com>
2024-03-23 21:01:23 +0900

    docs : 키워드 컨트롤러 swagger api 설명 추가

--- src/main/java/com/univ/haksamo/domain/keyword/controller/KeywordController.java
+++ src/main/java/com/univ/haksamo/domain/keyword/controller/KeywordController.java
@@ -3,15 +3,20 @@ package com.univ.haksamo.domain.keyword.controller;
 import com.univ.haksamo.domain.keyword.service.KeywordReadService;
 import com.univ.haksamo.domain.keyword.service.res.UserKeywordsDto;
 import com.univ.haksamo.global.format.success.SuccessResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "키워드 관련 api")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("${api.prefix.user}")
 public class KeywordController {
     private final KeywordReadService keywordReadService;
+
+    @Operation(summary = "즐겨찾기한 키워드 조회 api")
     @GetMapping("/users/{userId}/keywords")
     public SuccessResponse<UserKeywordsDto> keywordsWithSelected(@PathVariable Long userId){
         return new SuccessResponse(keywordReadService.getUserKeywords(userId));
